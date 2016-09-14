d48063a9819b4f6d86a03f8382dd37b033d3bc43
NicolasGraph <contact@nicolasmorand.com>
2016-09-14 09:53:31 +0200

    Adds code comments

--- src/tags.php
+++ src/tags.php
@@ -1,7 +1,6 @@
 <?php
 
-/**
- * Main plugin tag
+/*
  * Display a video
  */
 function oui_video($atts, $thing)
@@ -11,21 +10,32 @@ function oui_video($atts, $thing)
     // Instanciate Oui_video.
     $oui_video = new Oui_Video;
 
-    // Set tag attributes.
+    /*
+     * Set and check Tag attributes
+     */
+
+    // Get tag attributes.
     $get_atts = $oui_video->getAtts(__FUNCTION__);
+
+    // Set the array to be used by latts()
     foreach ($get_atts as $att => $options) {
         $get_atts[$att] = $options['default'];
     }
+
+    // Set tag attributes.
     extract(lAtts($get_atts, $atts));
 
     // Look for wrong attribute values.
     $oui_video->checkAtts(__FUNCTION__, $atts);
 
     /*
-     * Define the video provider and the video id.
+     * Get video infos
      */
+
+    // Check if the video is recognize as a video url.
     $match = $oui_video->videoInfos($video);
 
+    // Check if the video was recognize as a video url.
     if (!$match) {
         $provider = $provider ? $provider : get_pref('oui_video_provider');
         $custom = $video ? $video : strtolower(get_pref('oui_video_custom_field'));
@@ -37,11 +47,20 @@ function oui_video($atts, $thing)
     }
 
     /*
-     * Define player src and parameters.
+     * Get player Infos
      */
+
+    // Define which src to use for Youtube.
     $no_cookie ?: $no_cookie = get_pref('oui_video_youtube_no_cookie');
+
+    // Returns player infos
     $oui_video_provider = 'Oui_Video_' . $provider;
     $player_infos = (new $oui_video_provider)->playerInfos($provider, $no_cookie);
+
+    /*
+     * Prepare player parameters for the output
+     */
+
     $src = $player_infos['src'] . $video_id;
     $params = $player_infos['params'];
 
@@ -55,42 +74,50 @@ function oui_video($atts, $thing)
             $att_name = str_replace('-', '_', $param);
             $att = $$att_name;
 
-            // Because an input with color type is used in prefs
-            $param === 'color' ? $pref = str_replace('#', '', $pref) : '';
-
+            // Add modified attributes or prefs values as player parameters.
             if ($att === '' && $pref !== $default) {
-                // if the attribute is empty, get the related pref value.
+                // Remove # from the color pref as a color type is used for the pref input.
+                $param === 'color' ? $pref = str_replace('#', '', $pref) : '';
                 $used_params[] = $param . '=' . $pref;
             } elseif ($att !== '') {
+                // Remove the # in the color attribute just in case…
+                $att_name === 'color' ? $att = str_replace('#', '', $att) : '';
                 $used_params[] = $param . '=' . $att;
             }
         }
     }
 
-    /*
-     * Check if we need to append some parameters.
-     */
+    // Check if some player parameters has been used.
     if (!empty($used_params)) {
+        // Append them to the src.
         $src .= '?' . implode('&amp;', $used_params);
     }
 
     /*
-     * If the width and/or height has not been set
-     * we want to calculate new ones using the aspect ratio.
+     * Get the player size for the output
      */
+
+    // Set an array to be used to get the player size.
     $dims = array(
         'width' => $width,
         'height' => $width,
         'ratio' => $width,
     );
+
+    // Replace empty tag values by prefs.
     foreach ($dims as $dim => $value) {
         empty($value) ? $dims[$dim] = get_pref('oui_video_' .$dim) : '';
     }
 
     // Get the video size.
     $video_size = $oui_video->playerSize($dims);
+
     $width = $video_size['width'];
     $height = $video_size['height'];
+
+    /*
+     * Set the output
+     */
 
     $out = '<iframe
         width="' . $width . '"
@@ -103,8 +130,7 @@ function oui_video($atts, $thing)
     return doLabel($label, $labeltag).(($wraptag) ? doTag($out, $wraptag, $class) : $out);
 }
 
-/**
- * Conditional tag
+/*
  * Check a video url and its provider if provided.
  */
 function oui_if_video($atts, $thing)
@@ -113,7 +139,15 @@ function oui_if_video($atts, $thing)
 
     // Instanciate Oui_video.
     $oui_video = new Oui_Video;
+
+    /*
+     * Set and check Tag attributes
+     */
+
+    // Get tag attributes.
     $get_atts = $oui_video->getAtts(__FUNCTION__);
+
+    // Set the array to be used by latts()
     foreach ($get_atts as $att => $options) {
         $get_atts[$att] = $options['default'];
     }
@@ -124,12 +158,16 @@ function oui_if_video($atts, $thing)
     // Look for wrong attribute values.
     $oui_video->checkAtts(__FUNCTION__, $atts);
 
-    // Check if the plugin is able to catch any info from the provided video.
+    /*
+     * Get video infos
+     */
+
+    // Check if the video is recognize as a video url.
     $video_infos = $oui_video->videoInfos($video);
     $result = $video_infos ? $video_infos : $oui_video->videoInfos($thisarticle[strtolower($video)]);
 
+    // If the provider is provided check it too.
     if ($provider) {
-        // Is it the right provider?
         if ($provider === $result['provider']) {
             $result = $result ? true : false;
         } else {
